685dc4863455efac21bafb46a6165aef3ca9a6c3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-02-01 21:18:57 +0100

    actually add tables for the test position

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
             // 4k3/8/8/8/8/8/4Q3/4K3 b - - 0 1
             let pieces: [c_int; 6] = [
                 6,  // white king
-                1,  // white pawn
+                5,  // white queen
                 14, // black king
                 0,  // no piece
                 0,  // no piece
@@ -28,9 +28,9 @@ mod tests {
                 4,  // e1
                 12, // e2
                 60, // e8
-                64, // no square
-                64, // no square
-                64, // no square
+                0, // unused
+                0, // unused
+                0, // unused
             ];
             let dtm = prophet_tb_probe_dtm_dctx(
                 pieces.as_ptr(),
@@ -39,6 +39,10 @@ mod tests {
                 64, // no en-passant square
                 dctx,
             );
+            assert_eq!(dtm, -1001); // no tables added yet
+
+            assert!(11 <= prophet_tb_add_path(c"tables".as_ptr()));
+            let dtm = prophet_tb_probe_dtm_dctx(pieces.as_ptr(), squares.as_ptr(), 1, 64, dctx);
             assert_eq!(dtm, 16);
 
             prophet_tb_free_decompress_ctx(dctx);
